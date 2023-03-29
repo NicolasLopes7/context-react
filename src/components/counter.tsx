fd7ffc2add173c3b5e61c1b31682a13ead67dcf2
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2023-03-29 10:29:49 -0300

    initial commit

---
+++ src/components/counter.tsx
@@ -0,0 +1,5 @@
+export const Counter = () => {
+    return (
+        <span>Clicked {1} times</span>
+    )
+}
---
+++ src/components/increment-button.tsx
@@ -0,0 +1,5 @@
+export const IncrementButton = () => {
+    return (
+        <button>Increment</button>
+    )
+}
